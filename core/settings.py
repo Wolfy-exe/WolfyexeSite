c9e4a2297eb0e208fa3bdcf3f9603f4ea71e9076
Wolfy-exe <charlotte.rosseletnoll@gmail.com>
2024-05-29 01:23:36 -0300

    static root even

--- core/settings.py
+++ core/settings.py
@@ -116,6 +116,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static-files')
+
 STATIC_URL = 'static-files/'
 
 STATICFILES_DIRS = [
